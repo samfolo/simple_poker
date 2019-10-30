772948dba326f20b8a266c51bb1c8ee421c515a1
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-10-30 16:06:49 +0000

    Add get_random_card method and test

--- lib/simple_poker.rb
+++ lib/simple_poker.rb
@@ -1,11 +1,15 @@
 def create_deck
   deck = []
 
-  ["H", "C", "S", "D"].each { |suit|  
+  ["H", "C", "S", "D"].each { |suit|
     [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"].each { |num|
-      deck.push("#{num} of #{suit}") 
+      deck.push("#{num} of #{suit}")
     }
   }
 
   deck
+end
+
+def get_random_card
+  create_deck[rand(52)]
 end
--- spec/simple_poker_spec.rb
+++ spec/simple_poker_spec.rb
@@ -1,6 +1,6 @@
 require "simple_poker"
 
-RSpec.describe create_deck do 
+RSpec.describe create_deck do
 
   it "contains 52 cards" do
     expect(subject.length).to eq 52
@@ -8,19 +8,21 @@ RSpec.describe create_deck do
 
 end
 
-RSpec.describe "card" do
-  let(:card) { { suit: "Hearts", value: "King" } }
+RSpec.describe get_random_card do
 
-  it "has a value" do
-    expect(card[:suit]).to eq "Hearts"
-    expect(card[:value]).to eq "King"
+  it "gives random valid card" do
+    expect(create_deck.include?(subject)).to eq true
+  end
+
+  after do
+    puts subject
   end
 
 end
 
 # cards = {Hearts: [], Clubs: [], Spades: [], Diamonds: []}
 
-# cards.each { |k, v| 
+# cards.each { |k, v|
 #   v.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "k", "q", "j", "a")
 # }
 
