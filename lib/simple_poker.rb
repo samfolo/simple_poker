e41ebe688410ce17986156f53e3f1f3fe8251ae1
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-03 16:56:01 +0000

    finish cuts method

--- lib/simple_poker.rb
+++ lib/simple_poker.rb
@@ -14,14 +14,17 @@ def get_random_card
   create_deck[rand(52)]
 end
 
-
-# letters = ("a".."z").to_a
-# shuffled = []
-# while letters.length > 0
-#   ind = rand(letters.length - 1)
-#   shuffled << letters[ind]
-#   letters.delete_at(ind)
-# end
+letters = ("a".."z").to_a
+def random_shuffle(arr)
+  
+  shuffled = []
+  while arr.length > 0
+    ind = rand(arr.length)
+    shuffled << arr[ind]
+    arr.delete_at(ind)
+  end
+  shuffled.each { |i| print i, "\n" }
+end
 
 # puts shuffled
 letters2 = (1..52).to_a
@@ -35,7 +38,7 @@ def riffle(arr)
       shuffled2 << half[0]; half.delete_at(0)
     }
   end
-  print shuffled2, "\n"
+  # print shuffled2, "\n"
   shuffled2
 end
 
@@ -53,12 +56,22 @@ def cuts(arr)
   start = 0
   shuffled_arr = []
   while shuffled_arr.length != 52
-    chunk = rand(6) + start
+    chunk = rand(5) + start + 1
     shuffled_arr.unshift(arr[start .. chunk])
     start = chunk + 1
   end
-  shuffled_arr.reject(&:nil?)
+  shuffled_arr.reject { |i| i == [] || i.nil? }
 end
 
-cuts(create_deck)
+cut_deck = cuts(create_deck)
+i = 1
+while true
+  i += 1
+  if cuts(create_deck).include?([])
+    print cuts(create_deck).flatten, "\n\n  #{i} times"
+    break
+  end
+end
+
+# random_shuffle(cut_deck)
 # test
